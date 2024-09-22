e8d9fc6fcfe7544982959df19496af27f42fb1d8
harithac <harithanallagadda4905@gmail.com>
2024-09-22 21:03:44 +0530

    added expense files

---
+++ expense-1/variables.tf
@@ -0,0 +1,13 @@
+variable "instance_names" {
+  type        = list(string)
+  default     = ["mysql", "backend", "frontend"]
+  description = "instance names"
+}
+
+variable "zone_id" {
+  default = "Z100666436PQ6IK45WRZI"
+}
+
+variable "domain_name" {
+  default = "haritha.shop"
+}
